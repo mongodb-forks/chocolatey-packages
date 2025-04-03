91756a1ddfc759482748e369c11b864960b8d439
fmenezes <1765216+fmenezes@users.noreply.github.com>
2025-04-03 16:05:04 +0000

    AU: 1 updated - mongodb-atlas [skip ci]

--- automatic/mongodb-atlas/tools/chocolateyInstall.ps1
+++ automatic/mongodb-atlas/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
-﻿$url         = 'https://github.com/mongodb/mongodb-atlas-cli/releases/download/atlascli/v1.41.1/mongodb-atlas-cli_1.41.1_windows_x86_64.msi'
-$checksum    = '44369cd8a64ce9d753f21999e5683f9ecccb3c9fbeb619f18830706e278cdaf8'
+﻿$url         = 'https://github.com/mongodb/mongodb-atlas-cli/releases/download/atlascli/v1.41.2/mongodb-atlas-cli_1.41.2_windows_x86_64.msi'
+$checksum    = 'f0117c8e73f28392de095e79ee8b23fb71ddae7cc48ad621024605231d17c196'
 
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
